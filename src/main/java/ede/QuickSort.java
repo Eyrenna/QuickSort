58794a0f174858d937a1070bdc9bed04a0541192
Eyrenna <claudia365x@gmail.com>
2019-05-31 16:13:30 +0200

    first commit

---
+++ src/main/java/ede/QuickSort.java
@@ -0,0 +1,43 @@
+package ede;
+
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class QuickSort {
+
+    //Atributos
+
+    private Stack<Tarea> listaTareas = new Stack();
+
+    private ArrayList<Integer> numeros = new ArrayList<>();
+
+    private Integer pivot = null;
+
+
+    //Getters Setters
+
+
+    //Métodos
+
+    public Tarea crearTarea
+
+    public void hacerQS(ArrayList numeros) {
+
+        Tarea tarea = new Tarea(0, numeros.size()-1);
+
+        listaTareas.push(tarea);
+
+        while (!listaTareas.isEmpty()) {
+            Tarea actualTarea = listaTareas.pop();
+            if (actualTarea.getClass().)
+        }
+    }
+
+    public void hacerRecorrido(Tarea tarea) {
+
+
+    }
+
+
+}
---
+++ src/main/java/ede/Tarea.java
@@ -0,0 +1,26 @@
+package ede;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+public class Tarea {
+
+    private ArrayList<Integer> parte = new ArrayList<>();
+    private Integer posInicial = 0;
+    private Integer posFinal = 0;
+
+
+    //Constructor
+
+
+    public Tarea(ArrayList parte, Integer posInicial, Integer posFinal) {
+        this.parte = parte;
+        this.posInicial = posInicial;
+        this.posFinal = posFinal;
+    }
+
+    public ArrayList getParte() {
+        parte =
+        return parte;
+    }
+}
